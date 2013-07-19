e81b0cf5d3c6bf4c65d763825262da75090c6f04
Ryan Richardson <ryananthonyrichardson@gmail.com>
2013-07-19 00:26:57 -0700

    Insert working, Update Close

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+#!/usr/bin/python
+
 ## Program will create UPDATE, DELETE, or INSERT statements from one of:
-##  - CSV File
-##  - XLS File
-##  - XLSX File
+##  - CSV File - unimplemented
+##  - XLS File - unimplemented
+##  - XLSX File - in progress
 
-#file = open('../make-a-statement/
+# CAVEATS:
+#  - Program failed to parse file that I edited in LibreOffice. why?
+#  - XLS file renamed to XLSX did not work with openpyxl
 
-# Program failed to parse file that I edited in LibreOffice.
 
 from Tkinter import *
 from tkFileDialog import askopenfilename
@@ -13,18 +16,22 @@ from openpyxl.reader.excel import load_workbook
 from openpyxl.cell import Cell
 import sys
 
-update = 'UPDATE '
-uSet = ' SET '
-insert = 'INSERT INTO '
-delete = 'DELETE '
-where = ' WHERE '
-equals = ' = '
-tick = '\''
+
 
 def execute():
     sys.exit()
 
 def execute_xlsx(xlsxFile):
+    pre = ""
+    post = ""
+    statement = ""
+    update = "UPDATE "
+    uSet = " SET "
+    insert = "INSERT INTO "
+    delete = "DELETE "
+    where = " WHERE "
+    fields = [] # populates from the first row in the spreadsheet
+    
     wb = load_workbook(xlsxFile)
     sheetNames = wb.get_sheet_names() # sheetNames is Array of sheet names
     sheets = []
@@ -36,11 +43,40 @@ def execute_xlsx(xlsxFile):
 
     # at this point we have all worksheets stored in the sheets Array
 
+    # if operation = 1: #UPDATE
+    for i in range(0,len(sheets)):
+        for rIndex in range(0,len(sheets[i].rows)):
+            statement = statement + update + sheetNames[i] + uSet
+            for cIndex in range(0,len(sheets[i].columns)):
+                if rIndex == 0: #first row has the fields
+                    fields.append(sheets[i].cell(row=rIndex,column=cIndex).value)
+                else:
+                    statement = statement + str(fields[cIndex]) + " = '" + str(sheets[i].cell(row=rIndex,column=cIndex).value) + "'"
+                if cIndex != (len(sheets[i].columns) - 1): # more columns so add a comma
+                    statement = statement + ", "
+                if cIndex == (len(sheets[i].columns) - 1): # last column so insert line break
+                    # also add WHERE index value from user here
+                    # {WHERE fields[userSuppliedIndex] = sheets[i].cell(row=rIndex,column=userSuppliedIndex).value}
+                    statement = statement + "\n"
+                    
+    # else if operation = 2: #INSERT
     for i in range(0,len(sheets)):
         for rIndex in range(0,len(sheets[i].rows)):
             for cIndex in range(0,len(sheets[i].columns)):
-                # print update + sheetName[i] + uSet +
-                print sheets[i].cell(row=rIndex,column=cIndex).value
+                if rIndex == 0:
+                    if cIndex != (len(sheets[i].columns) - 1): # first row, but not last column
+                        pre = pre + str(sheets[i].cell(row=rIndex,column=cIndex).value) + ","
+                    else: # must be the last column, close paren and add literals
+                        pre = pre + str(sheets[i].cell(row=rIndex,column=cIndex).value) + ") VALUES ("
+                if cIndex != (len(sheets[i].columns) - 1): # not last, add comma
+                    post = post + "'" + str(sheets[i].cell(row=rIndex,column=cIndex).value) + "',"
+                else: # last columns of this row
+                    statement = statement + insert + sheetNames[i] + " (" + pre + post + "'" + str(sheets[i].cell(row=rIndex,column=cIndex).value) + "')\n"
+                    post = "" # reset post every row
+
+    statement.replace("'BULK'", "BULK")
+    print statement
+    
 
     print sheets[0].cell('A1').value
     #print sheets[0].cell(row=1,column=1).value
