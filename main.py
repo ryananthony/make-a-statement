11baa6f105119f0705b905cf2cd10ffe518cdaea
Ryan Richardson <ryananthonyrichardson@gmail.com>
2013-07-18 22:35:08 -0700

    Starting to Iterate the Data

--- main.py
+++ main.py
@@ -5,21 +5,55 @@
 
 #file = open('../make-a-statement/
 
+# Program failed to parse file that I edited in LibreOffice.
+
 from Tkinter import *
 from tkFileDialog import askopenfilename
 from openpyxl.reader.excel import load_workbook
+from openpyxl.cell import Cell
 import sys
 
+update = 'UPDATE '
+uSet = ' SET '
+insert = 'INSERT INTO '
+delete = 'DELETE '
+where = ' WHERE '
+equals = ' = '
+tick = '\''
 
 def execute():
     sys.exit()
+
+def execute_xlsx(xlsxFile):
+    wb = load_workbook(xlsxFile)
+    sheetNames = wb.get_sheet_names() # sheetNames is Array of sheet names
+    sheets = []
+    
+    for i in range(0,len(sheetNames)):
+        print sheetNames[i]
+        print i
+        sheets.append(wb.get_sheet_by_name(sheetNames[i]))
+
+    # at this point we have all worksheets stored in the sheets Array
+
+    for i in range(0,len(sheets)):
+        for rIndex in range(0,len(sheets[i].rows)):
+            for cIndex in range(0,len(sheets[i].columns)):
+                # print update + sheetName[i] + uSet +
+                print sheets[i].cell(row=rIndex,column=cIndex).value
+
+    print sheets[0].cell('A1').value
+    #print sheets[0].cell(row=1,column=1).value
+        
+    print sheets[0]
+    print filename + ' is an xlsx'
     
 
 def getFile():
     filename = askopenfilename()
 
     if filename[-5:] == '.xlsx':
-        print filename + ' is an xlsx'
+        execute_xlsx(filename)
     
     fileLabel.grid(row=0,columnspan=4)
     go.grid(row=2,column=3)
@@ -31,6 +65,7 @@ filename = ''
 
 
 root = Tk() # root widget, only 1 per program!
+root.wm_title("Make a Statement")
 f = Frame(root, height=100, width=300)
 f.grid_propagate(0)
 f.grid()
